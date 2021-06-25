c277b0eab36ba123462f8fe8b7119d6fe676ed04
Isaac Brown <37741511+ERSUCC@users.noreply.github.com>
2021-06-24 21:21:13 -0400

    suggested player 2 code
    i used a simple probability method to potentially increase chances of winning more matches (if i implemented it correctly lol)

--- player_2.py
+++ player_2.py
@@ -1,14 +1,34 @@
 import random
-#make the play function
-def play2(previous_play,play_history=[]):
-    #keep track of opponent's plays
-    play_history.append(previous_play)
-    #make an array of Play options
-    play_options=["R","P","S"]
-    #use information gathered to make a strategic choice
-    #---------------------------------------------------
-    #put smart people code here
-    #---------------------------------------------------
 
-    #or just pick randomly
-    return play_options[random.randint(0,2)]
+def play2(previous_play, play_history=[]):
+    play_history.append(previous_play)
+
+    play_options = ["R", "P", "S"]
+    counter = {"R": "P", "P": "S", "S": "R"}
+
+    r = 0
+    p = 0
+    s = 0
+
+    for play in play_history:
+        if play == "R":
+            r += 1
+
+        elif play == "P":
+            p += 1
+
+        else:
+            s += 1
+
+    max_played = max(r, p, s)
+
+    if max_played == r:
+        random_play = random.randint(1, 2)
+
+    elif max_played == p:
+        random_play = random.randint(0, 1) * 2
+
+    else:
+        random_play = random.randint(0, 1)
+
+    return counter[play_options[random_play]]
