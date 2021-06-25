5aaa89f6ea85bb4a737a314689ed486871490052
SeraphDev6 <82985907+SeraphDev6@users.noreply.github.com>
2021-06-25 08:10:35 -0400

    Update player_1.py
    Added additional functionality to machine learning algorithm, however it is still quite simple.

--- player_1.py
+++ player_1.py
@@ -1,14 +1,35 @@
 import random
 #make the play function
-def play1(previous_play,play_history=[]):
+def play1(previous_play,play_history=[],last_play=""):
     #keep track of opponent's plays
     play_history.append(previous_play)
+    #how many times we lost in a row(ties dont reset)
+    losses_in_a_row=0
     #make an array of Play options
     play_options=["R","P","S"]
+    counters={"R":"P","P":"S","S":"R"}
+    
     #use information gathered to make a strategic choice
     #---------------------------------------------------
     #put smart people code here
+    #check if we win or lose
+    if previous_play != "" and last_play!="":
+        if counters[previous_play]==last_play:
+            losses_in_a_row+=1
+        elif counters[last_play]==previous_play:
+            losses_in_a_row=0
+    #without enough data just play randomly
+    if len(play_history)>3:
+        #if we are on a winning streak keep using that strategy
+        if losses_in_a_row<=3:
+            last_play=counters[play_history[len(play_history)-2]]
+            return last_play
+        else:
+            last_play=play_options[random.randint(0,2)]
+            return last_play
+        
     #---------------------------------------------------
 
     #or just pick randomly
-    return play_options[random.randint(0,2)]
+    last_play=play_options[random.randint(0,2)]
+    return last_play
